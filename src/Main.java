cb20ddfafbc8462866370e8bc2ee026a2d0de9a1
christianmaierger <c.maier1122@gmail.com>
2020-12-14 23:56:24 +0100

    refactor, made code more readable trough extracting methods

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,85 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Path file = null;
 
+        file = getPathFromConsole(scanner, file);
+
+
+        String type = "";
+        type = getGranularityFromConsole(scanner, type);
+
+        boolean dgapAcceptable=false;
+        boolean dgap=false;
+        dgap = getIfUserWantsDgapFromConsole(scanner, dgapAcceptable, dgap);
+
+
+        IndexCreatorService indexCreator = new IndexCreatorService(file, type);
+                Index resultIndex = indexCreator.createIndexForDocument();
+
+                PrintService printService = new PrintService(resultIndex);
+
+
+                printService.printParametersAkaKenngrößen();
+
+
+
+                printService.printInvertedList(dgap);
+
+
+        }
+
+    private static boolean getIfUserWantsDgapFromConsole(Scanner scanner, boolean dgapAcceptable, boolean dgap) {
+        System.out.println("Please type in true if you want index with dgaps or false if you just want document numbers in inverted list!");
+        while(!dgapAcceptable) {
+
+            String input = scanner.nextLine();
+
+            try {
+                if (!input.equals("true") && !input.equals("false")) {
+
+                    System.out.println("Please just type in true or false!");
+                } else {
+                    if(input.equals("true")) {
+                        dgapAcceptable =true;
+                        dgap = true;
+                        break;
+                    } else {
+                        dgapAcceptable =true;
+                        dgap =false;
+                        break;
+                    }
+
+                }
+            } catch (Exception e) {
+                System.out.println("Sorry only paths to files allowed as input, you typed in: " + input);
+            }
+        }
+        return dgap;
+    }
+
+    private static String getGranularityFromConsole(Scanner scanner, String type) {
+        boolean typeAcceptable=false;
+        System.out.println("Please type line or verse to set document granularity!");
+        while(!typeAcceptable) {
+
+            String input = scanner.nextLine();
+
+            try {
+                if (!input.equals("line") && !input.equals("verse")) {
+                    System.out.println("Sry please just type in line or verse as second argument to decide if documents should be lines or verses");
+                } else {
+                    type =input;
+                    typeAcceptable = true;
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Sry please just type in line or verse as second argument to decide if documents should be lines or verses");
+            }
+        }
+        return type;
+    }
+
+
+    private static Path getPathFromConsole(Scanner scanner, Path file) {
         System.out.println("Please type in path to file you want to have indexed: ");
 
         boolean isAcceptableFile = false;
@@ -38,72 +117,8 @@ public class Main {
                 System.out.println("Sorry only paths to files allowed as input, you typed in: " + input);
             }
         }
-
-
-
-
-        String type = "";
-        boolean typeAcceptable=false;
-        while(!typeAcceptable) {
-            System.out.println("Please type line or verse to set document granularity!");
-            String input = scanner.nextLine();
-
-            try {
-                if (!input.equals("line") && !input.equals("verse")) {
-                    System.out.println("Sry please just type in line or verse as second argument to decide if documents should be lines or verses");
-                } else {
-                    type=input;
-                    typeAcceptable = true;
-                    break;
-                }
-            } catch (Exception e) {
-                System.out.println("Sry please just type in line or verse as second argument to decide if documents should be lines or verses");
-            }
-        }
-
-        boolean dgapAcceptable=false;
-        boolean dgap=false;
-        while(!dgapAcceptable) {
-            System.out.println("Please type in true if you want index with dgaps or false if you just want document numbers in inverted list!");
-            String input = scanner.nextLine();
-
-            try {
-                if (!input.equals("true") && !input.equals("false")) {
-
-                    System.out.println("Please just type in true or false!");
-                } else {
-                    if(input.equals("true")) {
-                        dgapAcceptable=true;
-                        dgap = true;
-                        break;
-                    } else {
-                        dgapAcceptable=true;
-                        dgap =false;
-                        break;
-                    }
-
-                }
-            } catch (Exception e) {
-                System.out.println("Sorry only paths to files allowed as input, you typed in: " + input);
-            }
-        }
-
-
-
-                IndexCreatorService indexCreator = new IndexCreatorService(file, type);
-                Index resultIndex = indexCreator.createIndexForDocument();
-
-                PrintService printService = new PrintService(resultIndex);
-
-
-                printService.printParametersAkaKenngrößen();
-
-
-
-                printService.printInvertedList(dgap);
-
-
-        }
-
+        return file;
     }
+
+}
 
